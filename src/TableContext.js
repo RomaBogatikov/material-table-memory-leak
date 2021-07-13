9e20e71d1af971a4343615c5c48c7b8da5afd7de
RomanBogatikov <ivaroman2000@gmail.com>
2021-07-12 20:32:16 -0400

    data generted on the server and pushed to client over websockets

--- src/TableContext.js
+++ src/TableContext.js
@@ -1,60 +1,23 @@
-import React, {useState, useEffect, useRef } from 'react';
-import Faker from 'faker';
-
-// copy-pasted from Dan Abramov's post: https://overreacted.io/making-setinterval-declarative-with-react-hooks/
-function useInterval(callback, delay) {
-  const savedCallback = useRef();
-  
-  // Remember the latest callback.
-  useEffect(() => {
-    savedCallback.current = callback;
-  }, [callback]);
-  
-  // Set up the interval.
-  // eslint-disable-next-line consistent-return
-  useEffect(() => {
-    function tick() {
-      savedCallback.current();
-    }
-    if (delay !== null) {
-      const id = setInterval(tick, delay);
-      return () => clearInterval(id);
-    }
-  }, [delay]);
-}
+import React, {useState, useEffect } from 'react';
+import io from 'socket.io-client';
 
 const TableContext = React.createContext();
 
-const TableContextProvider = ({children}) => {
-  
-  const generateUsers = () => {
-    const randomNumberOfUsers = 5 + Math.floor(Math.random() * 10);
-    const newUsers = [];
-    for (let i = 0; i < randomNumberOfUsers; i++) {
-      const user = {
-        title: Faker.name.title(),
-        name: Faker.name.firstName(),
-        surname: Faker.name.lastName(),
-        //email: Faker.internet.email(),
-        //phone: Faker.phone.phoneNumberFormat(),
-        //website: Faker.internet.domainName(),
-        //url: Faker.internet.url(),
-        birthYear: Faker.date.between(1950, 2000).getFullYear()
-        //birthCity: Faker.address.city()
-      };
-      
-      newUsers.push(user)
-    }
-    
-    return newUsers;
-  }
-  
-  const [data, setData] = useState(generateUsers());
 
-  useInterval(() => {
-    const newUsers = generateUsers();
-    setData(newUsers)
-  }, 300)
+const TableContextProvider = ({children}) => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const connection = io(`http://127.0.0.1:8082`, {
+      autoConnect: true,
+      reconnectionAttempts: 5,
+      transports: ['websocket'],
+    });
+
+    connection.on('users', (data) => {
+      const parsedData = JSON.parse(data);
+      setData(parsedData);
+    })
+  }, [])
 
   return <TableContext.Provider value={data}>{children}</TableContext.Provider>
 }
--- src/server.js
+++ src/server.js
@@ -1,16 +1,64 @@
 const express = require("express");
 const path = require('path');
 const app = express();
+const socketIo = require("socket.io");
+const http = require("http");
+const faker = require('faker');
 // Set port here
 const port = 8082;
 const staticFilesPath = path.resolve(__dirname, '../build');
 const staticFiles = express.static(staticFilesPath);
+
+const generateUsers = () => {
+  const randomNumberOfUsers = 5 + Math.floor(Math.random() * 10);
+  const newUsers = [];
+  for (let i = 0; i < randomNumberOfUsers; i++) {
+    const user = {
+      title: faker.name.title(),
+      name: faker.name.firstName(),
+      surname: faker.name.lastName(),
+      birthYear: faker.date.between(1950, 2000).getFullYear()
+    };
+
+    newUsers.push(user)
+  }
+
+  return newUsers;
+}
+
 app.use(staticFiles);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+
 // Home route
 app.get("/", (req, res) => res.sendFile(path.resolve(__dirname, '../build/index.html')));
 // 404
 app.use((_req, res) => res.status(404).send("Unable to find that!"));
-// Start server
-app.listen(port, () => console.log(`Server listening @ : http://localhost:${port}`));
+
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on("connection", (socket) => {
+  const emitData = () => {
+    const users = generateUsers();
+    io.emit('users', JSON.stringify(users));
+  }
+
+  const intervalId = setInterval(() => {
+    emitData();
+  }, 300)
+
+  socket.on("disconnect", () => {
+    clearInterval(intervalId);
+  })
+})
+
+server.listen(port, () => {
+  console.log(`Server listening @ : http://localhost:${port}`)
+})
+
+
+
+// // Start server
+// app.listen(port, () => console.log(`Server listening @ : http://localhost:${port}`));
